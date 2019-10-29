2d4a5d0bfabfbe728ec8af1aee6f453d963fea55
Coromoto Leon <cleon@ull.edu.es>
2019-10-29 19:26:47 +0000

    Refactorizando con un before :all

--- spec/punto_spec.rb
+++ spec/punto_spec.rb
@@ -1,25 +1,25 @@
 require 'lib/punto'
 
 describe Punto do
+	before (:all) do
+			@origen = Punto.new(0,0)
+			@unidad = Punto.new(1,1)
+	end
+
 	context "probando los getters y el to_s" do
 		it "se ha de poder instanciar un punto" do
-			origen = Punto.new(0,0)
-
-			expect(origen.x).to eq(0)
-			expect(origen.y).to eq(0)
+			expect(@origen.x).to eq(0)
+			expect(@origen.y).to eq(0)
 		end
 		it "el método to_s funciona bien" do
-			origen = Punto.new(0,0)
-
-			expect(origen.to_s).to eq("(0,0)")
+			expect(@origen.to_s).to eq("(0,0)")
 		end
 	end
 
 	context "probando los métodos de instancia" do
 		it "se puede multiplicar por un escalar" do
-			unidad = Punto.new(1,1)
 
-			expect((unidad * 5).to_s).to eq("(5,5)")
+			expect((@unidad * 5).to_s).to eq("(5,5)")
 		end
 	end
 end
