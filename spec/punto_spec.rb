9f62ad7cc9cd48cf11f86da3ef59c15a1edb96c4
Coromoto Leon <cleon@ull.edu.es>
2019-10-29 19:18:56 +0000

    usuario@ubuntu:~/tdd$ rspec -I. spec/punto_spec.rb ..
    Finished in 0.0046 seconds (files took 0.14965 seconds to load)
    2 examples, 0 failures

--- spec/punto_spec.rb
+++ spec/punto_spec.rb
@@ -3,11 +3,15 @@ require 'lib/punto'
 describe Punto do
 	context "probando los getters y el to_s" do
 		it "se ha de poder instanciar un punto" do
-			expect(@origen.x).to eq(0)
-			expect(@origen.y).to eq(0)
+			origen = Punto.new(0,0)
+
+			expect(origen.x).to eq(0)
+			expect(origen.y).to eq(0)
 		end
 		it "el método to_s funciona bien" do
-			expect(@origen.to_s).to eq("(0,0)")
+			origen = Punto.new(0,0)
+
+			expect(origen.to_s).to eq("(0,0)")
 		end
 	end
 end
